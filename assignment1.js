74ced8916fa9a50aac73cf87a5aa2cafc637ddf6
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-11-02 12:49:15 +0600

    create assignment1.js file for assignment 1

---
+++ assignment1.js
@@ -0,0 +1,29 @@
+// problem 1
+// ত োমাকে cubeNumber() নামে একটা function দেওয়া হয়েছে । functionটা একটা input নাম্বার নিবে ।
+// Task:
+// 1. input নাম্বারটাকে cube করে result টা রিটার্ন করবে ফাংশন থেকে ।
+// 2. Bonus: ইনপুট হিসেবে number টাইপ এর পরিবর্তে অন্য কিছু দিলে তুমি একটা মিন ংফুল মেসেজ রিটার্ন
+// করে দিবে ফাংশন থেকে ।
+
+function cubeNumber(number) {
+    
+    if ( typeof number !== "number"){
+        
+        return("enter a number");
+    }
+    
+    else{
+        const cube = Math.pow(number,3);
+        return cube;
+    }
+   
+
+}
+const input =100;
+const inputValue = cubeNumber(input);
+console.log(inputValue);
+
+ 
+
+
+
